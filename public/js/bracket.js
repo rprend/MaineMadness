664454d276d30506d21ce05d285aa44f307fafc6
Ryan Prendergast <rprendergast1121@gmail.com>
2018-03-14 22:31:18 -0500

    games started

--- public/js/bracket.js
+++ public/js/bracket.js
@@ -272,65 +272,65 @@ $(document).ready(function(){
 
 //COMMENT STARTING HERE ONCE GAMES START
 
-
-	  $(".slot").click(function() {
-
-      if (parseInt($(this).attr('data-col')) <= 5) {
-
-        var tmpTxtA = $("#c" + (parseInt($(this).attr('data-col')) + 1 ) + "s" + $(this).attr('data-game')).text();
-        var tmpColA = $(this).attr('data-col');
-
-        $("#c" + (parseInt($(this).attr('data-col')) + 1 ) + "s" + $(this).attr('data-game')).text($(this).text());
-
-        $('.slot').each(function() {
-            var text = $(this).text();
-            if (text == tmpTxtA) {
-              if ($(this).attr('data-col') > tmpColA && $(this).attr('data-col') <=6 ) {
-                $(this).text('');
-                $(this).id
-              }
-            }
-        });
-
-
-      } else if (parseInt($(this).attr('data-col')) >= 7 && parseInt($(this).attr('data-col')) < 12) {
-
-          var tmpTxtB = $("#c" + (parseInt($(this).attr('data-col')) - 1 ) + "s" + $(this).attr('data-game')).text();
-          var tmpColB = $(this).attr('data-col');
-
-          $("#c" + (parseInt($(this).attr('data-col')) - 1 ) + "s" + $(this).attr('data-game')).text($(this).text());
-
-          $('.slot').each(function() {
-              var text = $(this).text();
-              if (text == tmpTxtB) {
-                if (parseInt($(this).attr('data-col')) < tmpColB) {
-                    $(this).text('');
-                }
-              }
-          });
-
-      }
-
-      if (parseInt($(this).attr('data-col')) == 6) {
-        $("#c6s0").text($(this).text());
-      }
-
-      if (parseInt($(this).attr('data-col')) > 11) {
-        var tmpTxtC = $("#c" + (parseInt($(this).attr('data-col'))) + "s" + $(this).attr('data-game')).text();
-
-        $("#c" + (parseInt($(this).attr('data-col'))) + "s" + $(this).attr('data-game')).text($(this).text());
-        $("#ff" + $(this).attr('data-game')).text($(this).text());
-
-        $('.slot').each(function() {
-            var text = $(this).text();
-            if (text == tmpTxtC) {
-              if ($(this).attr('data-col') < 11) {
-                $(this).text('');
-              }
-            }
-        });
-      }
-    });
+    // 
+	  // $(".slot").click(function() {
+    //
+    //   if (parseInt($(this).attr('data-col')) <= 5) {
+    //
+    //     var tmpTxtA = $("#c" + (parseInt($(this).attr('data-col')) + 1 ) + "s" + $(this).attr('data-game')).text();
+    //     var tmpColA = $(this).attr('data-col');
+    //
+    //     $("#c" + (parseInt($(this).attr('data-col')) + 1 ) + "s" + $(this).attr('data-game')).text($(this).text());
+    //
+    //     $('.slot').each(function() {
+    //         var text = $(this).text();
+    //         if (text == tmpTxtA) {
+    //           if ($(this).attr('data-col') > tmpColA && $(this).attr('data-col') <=6 ) {
+    //             $(this).text('');
+    //             $(this).id
+    //           }
+    //         }
+    //     });
+    //
+    //
+    //   } else if (parseInt($(this).attr('data-col')) >= 7 && parseInt($(this).attr('data-col')) < 12) {
+    //
+    //       var tmpTxtB = $("#c" + (parseInt($(this).attr('data-col')) - 1 ) + "s" + $(this).attr('data-game')).text();
+    //       var tmpColB = $(this).attr('data-col');
+    //
+    //       $("#c" + (parseInt($(this).attr('data-col')) - 1 ) + "s" + $(this).attr('data-game')).text($(this).text());
+    //
+    //       $('.slot').each(function() {
+    //           var text = $(this).text();
+    //           if (text == tmpTxtB) {
+    //             if (parseInt($(this).attr('data-col')) < tmpColB) {
+    //                 $(this).text('');
+    //             }
+    //           }
+    //       });
+    //
+    //   }
+    //
+    //   if (parseInt($(this).attr('data-col')) == 6) {
+    //     $("#c6s0").text($(this).text());
+    //   }
+    //
+    //   if (parseInt($(this).attr('data-col')) > 11) {
+    //     var tmpTxtC = $("#c" + (parseInt($(this).attr('data-col'))) + "s" + $(this).attr('data-game')).text();
+    //
+    //     $("#c" + (parseInt($(this).attr('data-col'))) + "s" + $(this).attr('data-game')).text($(this).text());
+    //     $("#ff" + $(this).attr('data-game')).text($(this).text());
+    //
+    //     $('.slot').each(function() {
+    //         var text = $(this).text();
+    //         if (text == tmpTxtC) {
+    //           if ($(this).attr('data-col') < 11) {
+    //             $(this).text('');
+    //           }
+    //         }
+    //     });
+    //   }
+    // });
 
 //STOP COMMENT HERE WHEN GAMES STARTING
 
